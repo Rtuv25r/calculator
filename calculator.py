6fde11714146d1364baa578f19127ed063f52af5
Rtuv25r <150174284+Rtuv25r@users.noreply.github.com>
2025-02-12 21:40:09 +0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,25 @@
+def calculator():
+    print(" Calculator")
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    
+    print("Choose an operation: +, -, *, /")
+    operation = input("Enter operator: ")
+    
+    if operation == "+":
+        result = num1 + num2
+    elif operation == "-":
+        result = num1 - num2
+    elif operation == "*":
+        result = num1 * num2
+    elif operation == "/":
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            result = "Cannot divide by zero"
+    else:
+        result = "Invalid operation"
+    
+    print("Result:", result)
+
+calculator()
